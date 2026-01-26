056740af4f9dc92d313601d4050f42d31f9dcc40
doa30 <108628816+doa30@users.noreply.github.com>
2026-01-26 16:15:19 +0300

    - initial app setup

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def gallery():
     <!DOCTYPE html>
     <html>
     <head>
-        <title>LogNomaly Secure Cloud</title>
+        <title>Secure Cloud</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
             body { font-family: sans-serif; background: #222; color: white; text-align: center; }
